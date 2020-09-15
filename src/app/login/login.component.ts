e8dc4507754d95021d3df0a4e5a42289be90e8cc
Robert Steelheart <ralejsteelheart@sodimac.cl>
2020-09-15 00:06:08 -0300

    add basic login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,40 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  hide = true;
+  loginForm: FormGroup;
+  loading: boolean;
+  controlsAccessor: {
+    login: FormControl;
+    password: FormControl;
+  };
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initialState();
   }
 
+  initialState(): void {
+    // Setup form
+    this.loginForm = this.fb.group({
+      login: ['', [Validators.required, Validators.email]],
+      password: [null , [Validators.required, Validators.minLength(4)]]
+    });
+    // Set controls safe typed accessor
+    this.controlsAccessor = {
+      login: this.loginForm.get('login') as FormControl,
+      password: this.loginForm.get('password') as FormControl
+    };
+  }
+
+  login(): void {
+    console.log('login');
+    this.loading = true;
+    this.controlsAccessor.password.disable();
+    this.controlsAccessor.login.disable();
+    setTimeout(() => {
+      this.loading = false;
+    }, 3000);
+  }
 }
